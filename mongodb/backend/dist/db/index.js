5766189e74e67d201db4c6b52a76dc808ca0fee3
sheninthjr <sheninthjr23@gmail.com>
2025-01-22 21:45:50 +0530

    Gitignore added

--- mongodb/backend/dist/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-const mongoose_1 = require("mongoose");
-const userSchema = new mongoose_1.Schema({
-    id: mongoose_1.Schema.ObjectId,
-    title: String
-});
-exports.User = (0, mongoose_1.model)('User', userSchema);
